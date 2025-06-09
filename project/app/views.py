d2c8b27b3fedbd581ae042a75134408bd43619de
Tordi-portfolio <164408531+Tordi-portfolio@users.noreply.github.com>
2025-06-09 06:13:32 +0100

    Add files via upload

---
+++ project/app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
+from django.contrib import messages
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid credentials")
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def base(request):
+    return render(request, 'base.html', {})
+
+def dashboard(request):
+    return render(request, 'dashboard.html', )
